e62d6101366b239a50cd38bc23d21f4712eb0d53
ksv <kamal182006@outlook.com>
2025-08-21 23:29:01 +0530

    added new html layout file and designed some basic routes

--- app.py
+++ app.py
@@ -5,3 +5,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/add_deck')
+def add_card():
+    return render_template('add_deck.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/statistics')
+def statistics():
+    return '<h1>TODO</h1>'
